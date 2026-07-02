dd1c35f0e3b63a0abc5de3ab740e653ae57b946b
League Student <student@jointheleague.org>
2026-07-02 13:12:01 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/30_Turtle_Tricks/10_Turtle_Tricks.py
+++ lessons/10_Turtles/30_Turtle_Tricks/10_Turtle_Tricks.py
@@ -14,9 +14,9 @@ import turtle                           # Tell Python we want to work with the t
 turtle.setup(600, 600, 0, 0)            # Set the size of the window
 tina = turtle.Turtle()                  # Create a turtle named tina
 tina.forward(80)
-tina.left(260)
+tina.left(250)
 tina.forward(80)
-tina.left(235)
+tina.left(236)
 tina.forward(80)
 # Use tina.forward() and tina.left() to draw a triangle
 # Make each side of the triangle a different color with 
